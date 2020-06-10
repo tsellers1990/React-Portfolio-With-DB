8b164c2047c3dc0669a46dfcc92b53eca2f9d6ac
Tymothy Sellers <tsellers1990@gmail.com>
2020-06-10 16:47:25 -0600

    added some more template info and then styled the home page to a passable state

--- client/src/pages/Home/homepage.js
+++ client/src/pages/Home/homepage.js
@@ -12,8 +12,11 @@ class Home extends React.Component {
             </div>
             <div>
                 <img className="myFace" src={picOfMe} alt="Tymothy Sellers" />
-                <p className="paragraphText">
+                <p className="myBioText">
                 Experienced Photographer and Web Developer with a demonstrated history of working in the business supplies and equipment industry. Skilled in Operations Management, Management, Project Finance, Direct Sales, and Entrepreneurship. Strong operations professional graduated from University of Northern Colorado. 
+                Experienced Photographer and Web Developer with a demonstrated history of working in the business supplies and equipment industry. Skilled in Operations Management, Management, Project Finance, Direct Sales, and Entrepreneurship. Strong operations professional graduated from University of Northern Colorado. 
+                Experienced Photographer and Web Developer with a demonstrated history of working in the business supplies and equipment industry. Skilled in Operations Management, Management, Project Finance, Direct Sales, and Entrepreneurship. Strong operations professional graduated from University of Northern Colorado. 
+                Experienced Photographer and Web Developer with a demonstrated history of working in the business supplies and equipment industry. Skilled in Operations Management, Management, Project Finance, Direct Sales, and Entrepreneurship. Strong operations professional graduated. 
                 </p>
             </div>
             </>
