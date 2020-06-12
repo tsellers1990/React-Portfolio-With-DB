7a51d67e273e3627d129d2a26c6a11f1f57420e7
Tymothy Sellers <tsellers1990@gmail.com>
2020-06-12 16:27:13 -0600

    working on some redirect functionality on the homepage, I'm not sure exactly how to send the links through. that being said, I refuse to use more than one function for this.

--- client/src/pages/Home/homepage.js
+++ client/src/pages/Home/homepage.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './Home.css';
 import picOfMe from "../../Images/myface.JPG"
 // idk if I'll need more imports, should be fine but if I do add them here!
+var gitHubLink="https://github.com/tsellers1990"
+var linkedInLink="https://www.linkedin.com/in/BrokeEntrepreneur/"
+
+function imageClick() {
+    alert("This functionality is still under construction :D check back soon to see it working!")
+    // window.open(this.link)
+    //add logic here to show what portfolio pieces get hit in the db
+}
 
 class Home extends React.Component {
     render() {
@@ -12,12 +20,12 @@ class Home extends React.Component {
             </div>
             <div>
                 <img className="myFace" src={picOfMe} alt="Tymothy Sellers" />
-                <p className="myBioText">
-                Experienced Photographer and Web Developer with a demonstrated history of working in the business supplies and equipment industry. Skilled in Operations Management, Management, Project Finance, Direct Sales, and Entrepreneurship. Strong operations professional graduated from University of Northern Colorado. 
-                Experienced Photographer and Web Developer with a demonstrated history of working in the business supplies and equipment industry. Skilled in Operations Management, Management, Project Finance, Direct Sales, and Entrepreneurship. Strong operations professional graduated from University of Northern Colorado. 
-                Experienced Photographer and Web Developer with a demonstrated history of working in the business supplies and equipment industry. Skilled in Operations Management, Management, Project Finance, Direct Sales, and Entrepreneurship. Strong operations professional graduated from University of Northern Colorado. 
-                Experienced Photographer and Web Developer with a demonstrated history of working in the business supplies and equipment industry. Skilled in Operations Management, Management, Project Finance, Direct Sales, and Entrepreneurship. Strong operations professional graduated. 
-                </p>
+                <div className="bioBlock myBioText">
+                    <p link={gitHubLink} onClick={imageClick}>Find me on GitHub at:  {gitHubLink}</p>
+                    <p link={linkedInLink} onClick={imageClick}>Find me on LinkedIn at:  {linkedInLink}</p>
+                    <p> Short Bio: I love to solve coding challenges and take pictures of cars.</p>
+                    <p>Experienced Photographer and Web Developer with a demonstrated history of working in the business supplies and equipment industry. Skilled in Operations Management, Management, Project Finance, Direct Sales, and Entrepreneurship. Full Stack / Flex Web Dev that enjoys writing code and solving problems.</p>
+                </div>
             </div>
             </>
         )
