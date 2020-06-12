07212b9b3f36233669dd87c9defa44d15e232ccc
Tymothy Sellers <tsellers1990@gmail.com>
2020-06-12 15:40:23 -0600

    filled out descriptions and titles for the portfolio page. I'll need to find or create a 4th portfolio piece still.

--- client/src/pages/Portfolio/Portfolio.js
+++ client/src/pages/Portfolio/Portfolio.js
@@ -12,19 +12,19 @@ var item1 = {
     name: "Picture-It-Done",
     image: item1Image,
     link: "https://picture-it-done.herokuapp.com/",
-    description: "temp description"
+    description: "This is my strongest portfolio piece, Picture-it-Done is designed to connect homeowners (in need of house maintenance or repairs) with contractors that can do repairs for them. While still in an MVP state, users can use Auth0 to login and create job posts. AWS integration is still being built in so that the users will be able to upload photos to the site. Future features will include a chat function, the option for contractors to bid on the jobs, and a page for contractors to view their active/complete jobs."
 }
 var item2 = {
-    name: "Picture-It-Done",
+    name: "Dynamic Reading List",
     image: item2Image,
     link: "https://warm-crag-46689.herokuapp.com/",
-    description: "temp description"  
+    description: "I built this initially as a project for the bootcamp, but it's something that I actually would use. Users of the site can submit a book that they believe that I would read, it's then stored in a mongo database and I check back on it every few months when I run out of books to read."  
 }
 var item3 = {
-    name: "Picture-It-Done",
+    name: "(Public) Dynamic Budget",
     image: item3Image,
     link: "https://picture-it-done.herokuapp.com/",
-    description: "temp description"  
+    description: "This does exactly what it sounds like, It's a dynamic budget that updates the app every time I add money or subtract it. Future features include the ability to create an account so that your private financial information isn't widely available to the public!"  
 }
 var item4 = {
     name: "Picture-It-Done",
