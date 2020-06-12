6df5c7707a83accb52ad5c51218d835c48c91262
Tymothy Sellers <tsellers1990@gmail.com>
2020-06-12 17:07:23 -0600

    I realized that I had a placeholder =Placeholder and I removed it because I'm not lazy :)

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -15,7 +15,7 @@ const Form = (props) => {
                 <form className="col s12">
                     <div className="row">
                         <div className="input-field col s6">
-                            <input placeholder="Placeholder" id="first_name" type="text" className="validate" />
+                            <input id="first_name" type="text" className="validate" />
                             <label for="first_name">First Name</label>
                         </div>
                         <div className="input-field col s6">
