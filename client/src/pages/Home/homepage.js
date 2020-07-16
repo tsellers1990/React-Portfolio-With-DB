54590723bfe235e7a80b7d4855325029265f81c8
Tymothy Sellers <tsellers1990@gmail.com>
2020-07-15 18:51:38 -0600

    I made the front end look pretty cool!

--- client/src/pages/Home/homepage.js
+++ client/src/pages/Home/homepage.js
@@ -4,37 +4,39 @@ import Typist from 'react-typist'
 
 var gitHubLink = "https://github.com/tsellers1990";
 var linkedInLink = "https://www.linkedin.com/in/BrokeEntrepreneur/";
-const h1Text = "Hello, my name is Tymothy Sellers.";
-// let stringArray = [];
-// const onPage = [];
+const currentlyLiving = "Denver, Colorado";
+const messWithPeopleText = "Patience is a virtue"
 
-// function coolLetterPrint(string) {
-//   stringArray = string.split(""); //splits the string into an array of the letters
-// 
-//   for (var i = 0; i < stringArray.length; i++) {
-    //   
-    // let currentLetter = stringArray[i]; //setting letter to a var for scale
-// 
-    // setTimeout(function () { //slows the loop down for dramatic... effect
-    //   onPage.push(currentLetter);
-    //   console.log(onPage);
-    // }, i * 500);
-//   }
-// }
 
-// coolLetterPrint(h1Text);
+const buttonPress = () => {
+
+};
 
 class Home extends React.Component {
+    state= {
+        h1Text : "Hello, my name is Tymothy Sellers.",
+        bioText : `I graduated in early June of 2020 from the DU Coding Bootcamp Program and am currently ready to start working as a junior dev at your company! Currently based out of ${currentlyLiving}, I am ready and willing to relocate on a moments notice! I'm particularly interested in working in one of the main US hubs such as Austin, the San Francisco Bay area, New York, and Denver.`,
+        aboutMe : "Future nomad philanthropist/car guy/stoic philosopher",
+        finalLine : "Reach Out to tsellers1990@gmail.com if you'd like to contact me!",
+        delayAmount : '500'
+    }
   render() {
     return (
       <div className="wrapper">
         <div>
-            <Typist>
-            <h1 className="headerText">{h1Text}</h1>
-            <Typist.Delay ms={500} />
-            <div className="bioText">THIS IS the BNIOT </div>
+            <Typist >
+            <h1 className="headerText defaultText">{this.state.h1Text}</h1>
+
+            <Typist.Delay ms={this.state.delayAmount} />
+            <div className="bioText defaultText" >{this.state.bioText}</div>
+
+            <Typist.Delay ms={this.state.delayAmount} />
+            <div className="finalText defaultText" >{this.state.finalLine}</div>
             </Typist>  
         </div>
+        {/* <div className="nobodyCaresButton">
+            <button onClick={() => buttonPress()}>This button will do something someday</button>
+        </div> */}
       </div>
     );
   }
