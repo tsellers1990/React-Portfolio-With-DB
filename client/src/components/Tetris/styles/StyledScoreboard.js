93d3b35a0d0daf3b4a285d4f5967ecfc28a48eda
Tymothy Sellers <tsellers1990@gmail.com>
2020-08-16 12:16:39 -0600

    added some logic for the scores

--- client/src/components/Tetris/styles/StyledScoreboard.js
+++ client/src/components/Tetris/styles/StyledScoreboard.js
@@ -3,15 +3,15 @@ import styled from 'styled-components';
 export const StyledScoreboard = styled.div`
   box-sizing: border-box;
   display: flex;
-  align-items: top; 
+  align-items: flex-start; 
   margin: 0 0 20px 0;
   padding: 20px;
   border: 4px solid #333;
   min-height: 30px;
   width: 200%;
-  height: 100vh;
+  height: 61vh;
   border-radius: 20px;
-  color: ${props => (props.gameOver ? 'red' : '#999')};
+  color: ${'#999'};
   background: #000;
   font-family: Pixel, Arial, Helvetica, sans-serif;
   font-size: 0.8rem;
